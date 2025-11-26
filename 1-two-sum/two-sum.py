6ee28dcf0dd626659c460460b9cd0897b511386f
Jannat-ul-Mawa <114851352+Jannat-ul-Mawa20@users.noreply.github.com>
2025-11-26 16:15:43 +0500

    Time: 1 ms (62.82%) | Memory: 13.1 MB (76.85%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -7,33 +7,33 @@ class Solution(object):
         """
 
         # brute force approach
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                # check whether the first [i] and second [j] equals to target
-                if nums[i] + nums[j] == target:
-                    return [i , j]
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         # check whether the first [i] and second [j] equals to target
+        #         if nums[i] + nums[j] == target:
+        #             return [i , j]
 
                     # time complexity O(n^2)
 
         # hash map approach
 
-        # # create an empty hash map
-        # seen_values = {}
+        # create an empty hash map
+        seen_values = {}
         
-        # # loop through each number in array with its index
-        # for current_index, current_number in enumerate(nums):
+        # loop through each number in array with its index
+        for current_index, current_number in enumerate(nums):
 
-        #     # calculate what number we need to reach the target
-        #     required_number = target - current_number
+            # calculate what number we need to reach the target
+            required_number = target - current_number
 
-        #     # check if we've already seen the number, we need
-        #     if required_number in seen_values:
+            # check if we've already seen the number, we need
+            if required_number in seen_values:
 
-        #         # return both indices
-        #         required_index = seen_values[required_number]
-        #         return [required_index, current_index]
+                # return both indices
+                required_index = seen_values[required_number]
+                return [required_index, current_index]
 
-        #     # haven't found the pair, yet
-        #     # store this number and its index for future reference
-        #     seen_values[current_number] = current_index 
+            # haven't found the pair, yet
+            # store this number and its index for future reference
+            seen_values[current_number] = current_index 
 
